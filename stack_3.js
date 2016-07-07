499d8d9f0f5f8b41e6d60982d92bdce82dc6a301
Dustin John Pfister <dustin.pfister@gmail.com>
2016-07-07 17:07:35 -0400

    tab to space of stack_3.js

--- stack_3.js
+++ stack_3.js
@@ -11,26 +11,28 @@ var stack = (function () {
 
         w : 2,
         h : 2,
-		d : 2,
+        d : 2,
         points : [],
-		
-		set : function(options){
-			
-			options = options === undefined ? {} : options;
-			
-			// defaults
-			options.w = options.w === undefined ? 2 : options.w;
-			options.h = options.h === undefined ? 2 : options.h;
-			options.d = options.d === undefined ? 2 : options.d;
-			options.val = options.val === undefined ? 0 : options.val;
-			
-			this.w = options.w;
-			this.h = options.h;
-			this.d = options.d;
-			
-			this.clearGrid(options.val);
-			
-		},
+
+        set : function (options) {
+
+            options = options === undefined ? {}
+
+             : options;
+
+            // defaults
+            options.w = options.w === undefined ? 2 : options.w;
+            options.h = options.h === undefined ? 2 : options.h;
+            options.d = options.d === undefined ? 2 : options.d;
+            options.val = options.val === undefined ? 0 : options.val;
+
+            this.w = options.w;
+            this.h = options.h;
+            this.d = options.d;
+
+            this.clearGrid(options.val);
+
+        },
 
         clearGrid : function (val) {
 
@@ -38,17 +40,18 @@ var stack = (function () {
             len = this.w * this.h * this.d;
 
             this.points = [];
-			
-			if(val === undefined){ val = 0; }
-			
+
+            if (val === undefined) {
+                val = 0;
+            }
             while (i < len) {
 
                 this.points[i] = {
 
                     i : i,
                     x : i % this.w,
-                    y : Math.floor(   (i - (   Math.floor(i / (this.w * this.h)) * (this.w * this.h)    ))       / this.w),
-					z : Math.floor(i / (this.w * this.h)),
+                    y : Math.floor((i - (Math.floor(i / (this.w * this.h)) * (this.w * this.h))) / this.w),
+                    z : Math.floor(i / (this.w * this.h)),
                     val : val
 
                 };
@@ -58,51 +61,53 @@ var stack = (function () {
             }
 
         },
-		
-		countVal : function(theVal){
-			
-			var i = 0,
+
+        countVal : function (theVal) {
+
+            var i = 0,
             len = this.w * this.h * this.d,
-			count = 0;
-			
+            count = 0;
             while (i < len) {
 
-			    if(this.points[i].val === theVal){
-					
-					count += 1;
-					
-				}
+                if (this.points[i].val === theVal) {
+
+                    count += 1;
+
+                }
 
                 i += 1;
 
             }
-			
-			return count;
-			
-		},
-		
-		getPoint : function(ix, y, z){
-			
-			if(y === undefined){
-				
-				return this.points[ix];
-				
-			}else{
-				
-				//return this.points[y * this.w + ix];
-				
-				if(z === undefined){ z = 0;}
-				
-				return this.points[z * (this.w * this.h) + y * this.w + ix  ];
-				
-			}
-			
-		}
-		
+
+            return count;
+
+        },
+
+        getPoint : function (ix, y, z) {
+
+            if (y === undefined) {
+
+                return this.points[ix];
+
+            } else {
+
+                //return this.points[y * this.w + ix];
+
+                if (z === undefined) {
+                    z = 0;
+                }
+
+                return this.points[z * (this.w * this.h) + y * this.w + ix];
+
+            }
+
+        }
+
     };
 
     api.clearGrid();
-	
+
     return api;
 
-}());
+}
+    ());
